1964d25969109e4d2b36ffee76f9d3c7718c9a26
Artem Cherenkov <art.cherenkov@gmail.com>
2023-12-05 01:54:23 +0300

    Add deocoding route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import fastify, { FastifyRequest } from "fastify";
 import dotenv from "dotenv";
 import cors from "@fastify/cors";
 
@@ -9,8 +9,28 @@ const YANDEX_MAPS_API_KEY = process.env.YANDEX_MAPS_API_KEY;
 const server = fastify({ logger: true });
 
 server.register(cors, {
-  origin: true
+  origin: true,
 });
+
+server.post(
+  "/geocode",
+  async (
+    request: FastifyRequest<{ Body: { coords: [number, number] } }>,
+    reply,
+  ) => {
+    const coords = request.body.coords.join(",");
+
+    const baseUrl = "https://geocode-maps.yandex.ru/1.x/";
+    const response = await fetch(
+      baseUrl + `?apikey=${YANDEX_MAPS_API_KEY}&geocode=${coords}&format=json`,
+      { headers: { Referer: "http://localhost:3030/" } },
+    );
+
+    const data = await response.json();
+
+    return reply.send(data);
+  },
+);
 
 server.get("/load-ymaps", async (_request, reply) => {
   const response = await fetch(
